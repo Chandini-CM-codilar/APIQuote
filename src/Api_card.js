87cefd31adca305cec7ae6b3861efccdcebbb865
Chandini-CM-codilar <94517097+Chandini-CM-codilar@users.noreply.github.com>
2021-12-27 17:31:59 +0530

    Update Api_card.js

--- src/Api_card.js
+++ src/Api_card.js
@@ -5,39 +5,47 @@ class Api_card extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            posts: []
+            posts: [],
+            status: false
         };
 
+    }
+    componentDidMount(){
+        this.newQuote()
     }
     newQuote = () => {
         fetch('https://movie-quote-api.herokuapp.com/v1/quote/')
             .then(resp => resp.json())
-            .then(resp => this.setState({ posts: [resp] }))
+            .then(resp => this.setState({ posts: resp,
+            status: true
+            }));
+
 
     }
 
+
     render() {
+        console.log(this.state.status);
 
         return (
             <div className='Main_card'>
                 <div className='Main_color'>
-               
+                    
+
 
                     <div className='main-btn'>
                         <img onClick={this.newQuote} src="button.png" alt="button" />
 
-                        <img className='main-btn2' onClick={this.newQuote} src="left.png" alt="button" /></div>
+                        <img className='main-btn2' onClick={this.newQuote} src="left.png" alt="button" />
 
-                    {this.state.posts.map((posts, i) => {
-                        return (
-                            <p key={i}>
-                                {posts.quote}</p>
+                    </div>
+                    {this.state.status ?(<div className='Main_quote'>
 
+<img className="Main_quote" onClick={this.newQuote} src="quote.png" alt='quote_image' />
 
-                        )
-
-
-                    })}
+</div>):(<></>)}
+<p>
+                    {this.state.posts.quote}</p>
                 </div>
             </div>
 
